932e33697ad687aae8944f7028c3aa1b6740e709
Isuru B. Ranapana <raaisurub.ranapana@gmail.com>
2020-05-08 19:44:49 +0530

    Started

---
+++ korian/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:korian/app_screens/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Korian',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Login(),
+    );
+  }
+}
